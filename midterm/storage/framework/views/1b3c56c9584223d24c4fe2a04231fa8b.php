522382daed827a879991e6dd9e47b26dadaa4a46
Nashwa Aulia <98628350+nanaaapid@users.noreply.github.com>
2025-10-21 13:56:53 +0700

    Add files via upload

---
+++ midterm/storage/framework/views/1b3c56c9584223d24c4fe2a04231fa8b.php
@@ -0,0 +1,11 @@
+
+<?php $__env->startSection('title', 'Daftar Favorit'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="text-center">
+    <h1 class="text-3xl font-bold text-green-800">Halaman Favorit</h1>
+    <p class="text-gray-600 mt-3">Daftar gunung favorit akan ditampilkan di sini.</p>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\tiket_pendakian\resources\views/pages/home.blade.php ENDPATH**/ ?>
